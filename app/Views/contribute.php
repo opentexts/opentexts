2943188e7a49f06771761ff8c0f9b7499df39617
dg001sl <dg001sl@S-LEWIS-S10.natlib_1.nls.uk>
2020-05-10 15:24:35 +0100

    Updating CSV details

--- app/Views/contribute.php
+++ app/Views/contribute.php
@@ -7,4 +7,8 @@
             <a href="mailto:stuart.lewis@nls.uk">stuart.lewis@nls.uk</a>
             for details.
         </p>
+        <p>
+            You will need to be able to submit data as a CSV file. Details are
+            available on request.
+        </p>
 </section>
