cbd2cfaca9e499a69ac501cea92847f8457675fa
dg001sl <dg001sl@S-LEWIS-S10.natlib_1.nls.uk>
2020-05-12 19:41:44 +0100

    Basic search box

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,9 +1,13 @@
 <!-- CONTENT -->
 <section>
 
-	<h1>Search...</h1>
+    <h1>Search...</h1>
 
-
+    <form action="search" method="GET">
+        <p align="center">        
+            <input type="text" name="q" /> <button type=button">Search</button>
+        </p>    
+    </form>
 
 </section>
 
