0f9dd1e79e48138312bee2c48b9cb0848d398f44
dg001sl <dg001sl@S-LEWIS-S10.natlib_1.nls.uk>
2020-05-19 12:21:24 +0100

    Bootstrap components for facets and pagination

--- app/Views/search.php
+++ app/Views/search.php
@@ -7,32 +7,34 @@
         </p>    
     </form>
 
-    <p style="background-color:#f2f2f2;">
+    <?php
+        // Result count
+        if ($resultcount == 0) {
+            ?><div class="alert alert-success">There were <?= esc($resultcount); ?> records found</div><?php  
+        } else if ($resultcount == 1) {
+            ?><div class="alert alert-success">There was <?= esc($resultcount); ?> record found:</div><?php 
+        } else {
+            ?><div class="alert alert-success">There were <?= esc($resultcount); ?> records found:</div><?php
+        }
+    ?>
+    
+    <div class="alert alert-secondary">
         <b>Source:</b><br />
         <?php
             // Collection facet counts
             foreach ($collectionfacet as $value => $count) {
                 if ($count > 0) {
-                    ?><a href="/search/?q=<?= esc($q); ?>&collection=<?= esc($value); ?>"><?= esc($value); ?></a> [<?= esc($count); ?>] <?php
+                    ?><a href="/search/?q=<?= esc($q); ?>&collection=<?= esc($value); ?>"><?= esc($value); ?></a> <span class="badge badge-pill badge-primary"><?= esc($count); ?></span> <?php
                         if (!empty($collection)) {
-                            ?><a href="/search/?q=<?= esc($q); ?>">(remove)</a><?php 
+                            ?><a href="/search/?q=<?= esc($q); ?>" class="badge badge-danger">Remove</a><?php 
                         }
                     ?><br/><?php
                 }
             }
         ?>
-    </p>
+    </div>
 
     <?php
-        // Result count
-        if ($resultcount == 0) {
-            ?><p>There were <?= esc($resultcount); ?> records found</p><?php  
-        } else if ($resultcount == 1) {
-            ?><p>There was <?= esc($resultcount); ?> record found:</p><?php 
-        } else {
-            ?><p>There were <?= esc($resultcount); ?> records found:</p><?php
-        }
-
         // Results
         foreach ($results as $document) {
             ?><p>
@@ -55,24 +57,45 @@
     ?>
 
     <?php
-        // Do we need to show first page jump link
-        if ($start > 10) {
-            ?><a href='<?= $url; ?>'>&lt;&lt;</a>&nbsp;&nbsp;&nbsp;&nbsp;<?php
-        }
+        // Do we ened pagination?
+        if ($resultcount > 10) {
+            ?>
+        
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+            
+            <?php
+            // Do we need to show first page jump link
+            if ($start > 10) {
+                ?><li class="page-item"><a class="page-link" href='<?= $url; ?>'>
+                        <span aria-hidden="true">&laquo;</span>
+                        <span class="sr-only">Previous</span></a>
+                  </li><?php
+            }
 
-        // Do we need to show previous page link?
-        if ($start >= 10) {
-            ?><a href='<?= $url . '&start=' . ($start - 10); ?>'>&lt; Previous page</a>&nbsp;&nbsp;&nbsp;&nbsp;<?php
-        }
-        
-        // Do we need to show next page link?
-        if ($resultcount > $start + 10) {
-            ?><a href='<?= $url . '&start=' . ($start + 10); ?>'>Next page &gt;</a>&nbsp;&nbsp;&nbsp;&nbsp;<?php
-        }
-        
-        // Do we need to show a last page jump link
-        if ($start < ($resultcount - 10)) {
-            ?><a href='<?= $url . '&start=' . (floor($resultcount / 10) * 10); ?>'>&gt;&gt;</a><?php
+            // Do we need to show previous page link?
+            if ($start >= 10) {
+                ?><li class="page-item"><a class="page-link" href='<?= $url . '&start=' . ($start - 10); ?>'>&lt; Previous page</a></li><?php
+            }
+
+            // Do we need to show next page link?
+            if ($resultcount > $start + 10) {
+                ?><li class="page-item"><a class="page-link" href='<?= $url . '&start=' . ($start + 10); ?>'>Next page &gt;</a></li><?php
+            }
+
+            // Do we need to show a last page jump link
+            if ($start < ($resultcount - 10)) {
+                ?><li class="page-item"><a class="page-link" href='<?= $url . '&start=' . (floor($resultcount / 10) * 10); ?>'>
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Last</span></a>
+                  </li><?php
+            }
+            ?>
+                
+                </ul>
+            </nav>    
+                
+            <?php
         }
     ?>
         
