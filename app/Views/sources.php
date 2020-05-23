3e087877141132acad25972e4e46299cf722112d
Stuart Lewis <stuart@stuartlewis.com>
2020-05-23 19:26:09 +0100

    Updated contribution names

--- app/Views/sources.php
+++ app/Views/sources.php
@@ -4,6 +4,10 @@
     <ul>
         <li><a href="https://www.nls.uk/" target="_blank">The National Library of Scotland</a></li>
         <li><a href="https://library.wales/" target="_blank">The National Library of Wales</a></li>
-        <li>Add the rest...</li>
+        <li><a href="https://www.bodleian.ox.ac.uk/" target="_blank">Bodleian Libraries, University of Oxford</a></li>
+        <li><a href="https://www.lib.cam.ac.uk/" target="_blank">Cambridge University Library</a></li>
+        <li><a href="https://www.bl.uk/" target="_blank">The British Library</a></li>
+        <li><a href="https://wellcomecollection.org/" target="_blank">The Wellcome Collection</a></li>
+        <li>You? Learn how to <a href="/contribute">contribute</a></li>
     </ul>
 </main>
