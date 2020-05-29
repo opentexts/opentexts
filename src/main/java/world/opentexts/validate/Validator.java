928a5417f03354bc711c93db77aa3a9f0b955ecf
Stuart Lewis <stuart@stuartlewis.com>
2020-05-29 19:25:27 +0100

    Correcting duplicate class package name

--- src/main/java/world/opentexts/validate/Validator.java
+++ src/main/java/world/opentexts/validate/Validator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package world.opentexts.opentexts.validate;
+package world.opentexts.validate;
 
 import java.io.FileReader;
 import java.io.Reader;
