98628d77a0754a8fec8651bcb4aff0b28c553dfc
Stuart Lewis <stuart@stuartlewis.com>
2020-06-11 10:45:32 +0100

    Initial doorbell.io implementation

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -28,6 +28,19 @@
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        
+        <script type="text/javascript">
+            window.doorbellOptions = {
+                "id": "11688",
+                "appKey": "h4ZLt1kpIj6ch9JeeV48pgpl653MdPYKmpnOEZqDxqM3sZ6vgTuDmJAufJY8R8uG"
+            };
+            (function(w, d, t) {
+                var hasLoaded = false;
+                function l() { if (hasLoaded) { return; } hasLoaded = true; window.doorbellOptions.windowLoaded = true; var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/'+window.doorbellOptions['id']+'?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g); }
+                if (w.attachEvent) { w.attachEvent('onload', l); } else if (w.addEventListener) { w.addEventListener('load', l, false); } else { l(); }
+                if (d.readyState == 'complete') { l(); }
+            }(window, document, 'script'));
+        </script>
 
     </body>
 </html>
