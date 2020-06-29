4daaa3d27f4ffb7ab661705d4bc6af7a67431477
Stuart Lewis <stuart@stuartlewis.com>
2020-06-29 15:32:45 +0100

    Addition of analytics logging

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,7 +1,14 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <!-- Required meta tags -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171145480-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-171145480-1');
+        </script>
+        
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>OpenTexts.world: <?= esc($title); ?></title>
