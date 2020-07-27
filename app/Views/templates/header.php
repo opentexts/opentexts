cd72078a7cfea54426cd23a5206963e5a975b1a2
sarah semark <sarah@octopusthink.com>
2020-07-27 19:38:31 +0100

    feat: Simplify & style primary navigation.
    Also, remove references to Bootstrap.

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -14,8 +14,6 @@
         <title>OpenTexts.world: <?= esc($title); ?></title>
         <meta name="description" content="Search, discover, and download open digitised texts and books">
         <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/otw-base.css">
         <link rel="stylesheet" href="/css/app.css">
         <?php if (getenv('CI_ENVIRONMENT') !== 'production'): ?>
           <script src="//localhost:35729/livereload.js"></script>
@@ -24,23 +22,4 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
-      <a class="navbar-brand" href="/">OpenTexts.World</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExample04">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="/home">Home</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="/sources">Sources</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="/contribute">Contribute</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+<?php include('navigation-primary.php'); ?>
---
+++ app/Views/templates/navigation-primary.php
@@ -0,0 +1,16 @@
+<nav class="container mx-auto py-8">
+    <ul class="flex justify-center space-x-8">
+        <li>
+            <a href="/home">Home</a>
+        </li>
+        <li>
+            <a href="/about">About</a>
+        </li>
+        <li>
+            <a href="/contribute">Get Involved</a>
+        </li>
+        <li>
+            <a href="/help">Help</a>
+        </li>
+    </ul>
+</nav>
