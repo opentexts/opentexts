3007e8cf9a4bbf237cb5b4eb2410007d7dd06bc4
sarah semark <sarah@octopusthink.com>
2020-07-27 23:34:19 +0100

    chore: Remove some excess Bootstrap stuff we aren't using anymore.

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,18 +1,8 @@
-        <footer class="pt-4 my-md-5 pt-md-5 border-top">
-            <div class="container">
-              <div class="col">          
-                <h5>Information</h5>
-                <ul class="list-unstyled text-small">
-                  <li><a class="text-muted" href="/about">About</a></li>
-                  <li><a class="text-muted" href="/support">Support</a></li>
-                  <li><a class="text-muted" href="/contribute">Contribute data</a></li>
-                </ul>
-            </div>
+        <footer>
+
         </footer>
                 
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+       
         
         <script type="text/javascript">
             window.doorbellOptions = {
