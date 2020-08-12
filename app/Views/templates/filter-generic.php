c0924620f89671fb5efd77bc1128acc042861755
Brian Beacom <brian.beacom@outplay.com>
2020-08-12 18:46:30 +0100

    Add styling to filter views to start aligning with #13

--- app/Views/templates/filter-generic.php
+++ app/Views/templates/filter-generic.php
@@ -4,43 +4,44 @@ function render_facetset(string $label, string $key, ?string $currentValue,  Fac
 {
     $facetarray = $facet->getValues();
     ?>
-    <div tabindex="0" class="card-header bg-info inline-block outline-none" style="color: #333;">
+    <div tabindex="0" class="card-header bg-info inline-block outline-none cursor-pointer p-y-2 mr-4" style="color: #333;">
         <?= $label; ?>
         <input type="hidden" name="<?=$key?>" value="<?= $currentValue ?>"/>
     <div class="rounded-md absolute filter-dropdown overflow-y-auto" style="background-color: #DDD;">
-        <ul class="list-group list-group-flush ">
+        <ul class="list-group list-group-flush p-l-1 p-y-1">
         <?php
+
+            if($currentValue != null)
+            {
+                ?>
+                <li class="p-1" onclick="this.closest('form').<?=$key?>.value =''; this.closest('form').submit()">
+                    <span class="badge badge-pill badge-danger">All libraries</span>
+                </li>
+                <?php
+            }
             foreach ($facetarray as $value => $count) {
                 if ($count > 0) {
                     if($value == $currentValue)
                     {
                     ?>
-                        <li class="list-group-item" onclick="this.closest('form').<?=$key?>.value ='<?= esc($value) ?>'; this.closest('form').submit()">
+                        <li class="list-group-item bg-offWhite p-1" onclick="this.closest('form').<?=$key?>.value ='<?= esc($value) ?>'; this.closest('form').submit()">
                             <?= $value ?>
-                            <span class="badge badge-pill badge-primary" style="float: right"><?= number_format($count); ?></span>
+                            <!--span class="badge badge-pill badge-primary" style="float: right"><?= number_format($count); ?></span-->
                         </li>
                     <?php
                     }
                     else
                     {
                         ?>
-                        <li class="list-group-item selected" onclick="this.closest('form').<?=$key?>.value ='<?= esc($value) ?>'; this.closest('form').submit()">
+                        <li class="list-group-item p-1" onclick="this.closest('form').<?=$key?>.value ='<?= esc($value) ?>'; this.closest('form').submit()">
                             <?= $value ?>
-                            <span class="badge badge-pill badge-primary" style="float: right"><?= number_format($count); ?></span>
+                            <!-- span class="badge badge-pill badge-primary" style="float: right"><?= number_format($count); ?></span -->
                         </li>
                         <?php
                     }
                 }
             }
 
-            if($currentValue != null)
-            {
-                ?>
-                <li>
-                    <span class="badge badge-pill badge-danger" onclick="this.closest('form').<?=$key?>.value =''; this.closest('form').submit()">Remove</span>
-                </li>
-                <?php
-            }
         ?>
         </ul>
     </div>
--- app/Views/templates/search-results.php
+++ app/Views/templates/search-results.php
@@ -1,4 +1,22 @@
-<div class="card">
+<?php
+/** @var string $title */
+/** @var string $q */
+/** @var integer $resultcount */
+/** @var Solarium\Component\Result\Facet\FacetResultInterface $organisationfacet */
+/** @var Solarium\Component\Result\Facet\FacetResultInterface $languagefacet */
+/** @var Solarium\Component\Result\Highlighting\Highlighting $highlighted */
+/** @var  Solarium\Core\Query\Result\ResultInterface|Solarium\QueryType\Select\Result\Result $results */
+/** @var  string $selectedorganisation */
+/** @var  string $organisation */
+/** @var  string $selectedlanguage */
+/** @var  string $language */
+/** @var  integer $start */
+/** @var  integer $count */
+/** @var  integer $lastpage */
+/** @var  string $url */
+/** @var  string $exporturl */
+?>
+<div class="container mx-auto max-w-xl mb-8">
     <form>
         <input type="hidden" name="q" value="<?= $q ?>"/>
         <?php
