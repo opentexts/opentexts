92ca5f4f47db170fc2f9bbc5632c27443018298b
sarah semark <sarah@octopusthink.com>
2020-08-20 16:02:42 +0100

    feat: Improve focus styles for search input.

--- app/Views/templates/search-form.php
+++ app/Views/templates/search-form.php
@@ -2,7 +2,7 @@
     <label class="block w-full max-w-2xl text-white mb-1 pl-4 text-sm <?php echo isset($q) ? "sr-only" : "" ?>" for="search-input">Search by title, author, date, or subject</label>
 
     <div class="w-full bg-white max-w-2xl rounded-full flex focus:border-blue-500">
-        <input type="search" name="q" id="search-input" class="w-full max-w-2xl rounded-full py-4 px-6 text-lg text-gray-700 focus:outline-none border-2 focus:border-blue-500 focus:text-blue-700" value="<?php echo isset($q) ? esc($q) : "" ?>" />
+        <input type="search" name="q" id="search-input" class="w-full max-w-2xl rounded-full py-4 px-6 text-lg text-gray-700 focus:outline-none border-2 border-transparent focus:border-blue-500 focus:text-blue-700" value="<?php echo isset($q) ? esc($q) : "" ?>" />
 
         <button class="w-auto items-center text-blue-800 -ml-12 p-2 pr-4 hover:text-blue-500">
             <span><?php echo file_get_contents('svg/search.svg'); ?></span>
