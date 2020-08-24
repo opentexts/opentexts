42e444ea3d480631eda70a9992ec71501000cfe6
sarah semark <sarah@octopusthink.com>
2020-08-24 13:23:22 +0100

    feat: Add focus styling to search button.

--- app/Views/templates/search-form.php
+++ app/Views/templates/search-form.php
@@ -4,7 +4,7 @@
     <div class="w-full bg-white max-w-2xl rounded-full flex">
         <input name="q" type="search" id="search-input" class="w-full max-w-2xl py-2 px-2 my-2 ml-6 mr-12 text-lg text-gray-700 border-2 border-transparent focus:border-blue-300 focus:outline-none focus:text-blue-700" value="<?php echo isset($q) ? esc($q) : "" ?>" />
 
-        <button class="w-auto items-center text-blue-800 -ml-12 p-2 pr-4 hover:text-blue-500">
+        <button class="w-auto items-center text-blue-800 -ml-12 py-2 px-3 mr-2 my-2 rounded-full border-2 border-transparent hover:text-blue-500 focus:outline-none focus:border-blue-300">
             <span><?php echo file_get_contents('svg/search.svg'); ?></span>
             <span class="sr-only">Search</span>
         </button>
