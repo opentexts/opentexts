42a7e2b6d715f1c1199fa21920fc73c074145214
sarah semark <sarah@octopusthink.com>
2020-08-24 12:58:02 +0100

    feat: Improve search input alignment.
    Changing the padding around the search field allows us to better support built-in browser functionality, like the "x" clear input button in webkit-based browsers, at the expense of a small aesthetic cost (the focus styling isn't as elegant.)
    
    We might be able to fix this with some Javascript, and apply styling to the container element instead of the actual form input, but for now this seems like a reasonable compromise that prioritises accessibility and usability over sheer aesthetics.
    
    Close #64.

--- app/Views/templates/search-form.php
+++ app/Views/templates/search-form.php
@@ -1,8 +1,8 @@
 <form method="get" action="/search" class="container flex flex-col justify-center items-center">
     <label class="block w-full max-w-2xl text-white mb-1 pl-4 text-sm <?php echo isset($q) ? "sr-only" : "" ?>" for="search-input">Search by title, author, date, or subject</label>
 
-    <div class="w-full bg-white max-w-2xl rounded-full flex focus:border-blue-500">
-        <input type="search" name="q" id="search-input" class="w-full max-w-2xl rounded-full py-4 px-6 text-lg text-gray-700 focus:outline-none border-2 border-transparent focus:border-blue-500 focus:text-blue-700" value="<?php echo isset($q) ? esc($q) : "" ?>" />
+    <div class="w-full bg-white max-w-2xl rounded-full flex">
+        <input name="q" type="search" id="search-input" class="w-full max-w-2xl py-2 px-2 my-2 ml-6 mr-12 text-lg text-gray-700 border-2 border-transparent focus:border-blue-300 focus:outline-none focus:text-blue-700" value="<?php echo isset($q) ? esc($q) : "" ?>" />
 
         <button class="w-auto items-center text-blue-800 -ml-12 p-2 pr-4 hover:text-blue-500">
             <span><?php echo file_get_contents('svg/search.svg'); ?></span>
