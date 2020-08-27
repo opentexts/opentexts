3a1e4e1579994ec133edb9a3e0840995a01ba831
Stuart Lewis <stuart@stuartlewis.com>
2020-08-27 10:04:50 +0100

    Update public/scripts/search-results.js
    Co-authored-by: Brian Beacom <brizee@users.noreply.github.com>

--- public/scripts/search-results.js
+++ public/scripts/search-results.js
@@ -7,7 +7,7 @@ controller.onResultsAdded.addEventListener(function(){
     document.querySelector("#resultCount").innerText = (controller.getStart()+1) + "-" + controller.getCount();
     if (controller.getCount() >= controller.getTotal()) {   
         Array.prototype.forEach.call(document.getElementsByClassName("load-more-results"), function(element) {
-            element.className += " invisible";
+            element.classList.add("invisible");
         });
     }
 });
