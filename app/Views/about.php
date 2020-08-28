2a53b4f49a6c317caae13f5bab0e74a5cf4822eb
Stuart Lewis <stuart@stuartlewis.com>
2020-08-28 17:16:33 +0100

    About page: copyedit & improve design #81

--- app/Views/about.php
+++ app/Views/about.php
@@ -9,41 +9,60 @@
         </p>
 
         <p>
-            OpenTexts.World brings together, in a single search, the digitised collections of several major UK libraries. 
+            OpenTexts.World brings together, in a single search, the digitised collections of a group of libraries. 
             A simple search interface, retrieves brief information about digitised books and other texts, and provides users with links to  resources.
         </p>
 
         <p>
-            The service is based on research undertaken by the Global Digitised Data Network, an Anglo-American research collaboration, funded by the 
+            The service is based on research undertaken by the 
+            <a href="https://gddnetwork.arts.gla.ac.uk/index.php/about-us/" title="link to GDD Network">Global Digitised Data Network</a>,
+            an Anglo-American research collaboration, funded by the 
             <a href="https://ahrc.ukri.org/" title="AHRC">Arts and Humanities Research Council</a>.
         </p>
+                
+        <h2>Statistics</h2>
+        <p>
+            Currently the site provides access to:
+        </p>
+        <ul class="list-disc list-inside mb-2">
+            <li>Over 3.8 million records (more than 47 miles of books if they were all put on a long bookshelf!)</li>
+            <li>Items in 85 different languages</li>
+            <li>Items from as <a href='/search?q="gutenberg%20bible"&organisation=National%20Library%20of%20Scotland'>early as 1455</a> through to many more <a href="/search?q=2020">modern books</a></li>
+            <li>Links to different formats where they exist, such as PDF, plain text, and <a href="https://iiif.io/">IIIF manifests</a></li>
+        </ul>
     
         <h2>Sources</h2>
-
         <p>OpenTexts.World currently searches digitized content from the following libraries:</p>
         <ul class="list-disc list-inside mb-2">
-            <li><a href="https://www.nls.uk/">National Library of Scotland</a></li>
-            <li><a href="https://library.wales/">National Library of Wales</a></li>
-            <li><a href="https://www.bodleian.ox.ac.uk/">Bodleian Libraries, University of Oxford</a></li>
-        <!--<li><a href="https://www.lib.cam.ac.uk/">Cambridge University Library</a></li>
-            <li><a href="https://www.bl.uk/">The British Library</a></li>-->
-            <li><a href="https://wellcomecollection.org/">The Wellcome Collection</a></li>
-            <li>You? Learn how to <a href="/contribute">contribute</a></li>
+            <li><a href="https://archive.org/details/americana">Internet Archive American Libraries Collection</a> (<a href="/search?organisation=Internet%20Archive%20Books">3,182,419 items</a>)</li>
+            <li><a href="https://digitalnz.org/">DigitalNZ</a> (<a href="/search?organisation=DigitalNZ">244,376 items</a>)</li>
+            <li><a href="https://www.bodleian.ox.ac.uk/">Bodleian Libraries, University of Oxford</a> (<a href="/search?organisation=Bodleian%20Libraries">231,088 items</a>)</li>
+            <li><a href="https://wellcomecollection.org/">The Wellcome Collection</a> (<a href="/search?organisation=Wellcome%20Collection">182,576 items</a>)</li>
+            <li><a href="https://www.nls.uk/">National Library of Scotland</a> (<a href="/search?organisation=National%20Library%20of%20Scotland">26,971 items</a>)</li>
+            <li><a href="https://library.wales/">National Library of Wales</a> (<a href="/search?organisation=ational%20Library%20of%%20Wales">2,801 items</a>)</li>
+            <li>Your Library? Learn how to <a href="/contribute">contribute</a></li>
         </ul>
             
         <h2>Project Team</h2>
         <p>People responsible for this site:</p>
         <ul class="list-disc list-inside mb-2">
-            <li>Stuart Lewis (National Library of Scotland)</li>
-            <li>Gill Hamilton (National Library of Scotland)</li>
+            <li><a href="mailto:stuart.lewis@nls.uk">Stuart Lewis</a> (National Library of Scotland)</li>
+            <li><a href="mailto:gill.hamilton@nls.uk">Gill Hamilton</a> (National Library of Scotland)</li>
             <li>We've been grateful for assistance from the <a href="https://www.scottishtecharmy.org/">Scottish Tech Army</a>, including:
                 <ul>
                     <li>Design: Sarah Semark (<a href="https://octopusthink.com/">Octopus Think</a>)</li>
+                    <li>Programming: Brian Beacom</li>
                     <li>Data: Ali King</li>
                     <li>Project Management: Alec Davis</li>
                 </ul>
-            <li>Lots of others...</li>
+            </li>
+            <li>Our content providers who have provided millions of records of digitised texts</li>
             <li>You? Please get in touch!</li>
         </ul>
+        <p>
+            The code for the site is available openly at <a href="https://github.com/opentexts/opentexts/">https://github.com/opentexts/opentexts</a>
+            where you are also welcome to <a href="https://github.com/opentexts/opentexts/issues">report bugs</a> or to 
+            <a href="https://github.com/opentexts/opentexts/issues">make feature requests</a>.
+        </p>
     </article>
 </main>
