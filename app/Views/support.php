2e249edeb15a342f097daeecb91041e3b1976316
Stuart Lewis <stuart@stuartlewis.com>
2020-08-28 20:04:28 +0100

    Help page: expand content & improve design #7

--- app/Views/support.php
+++
@@ -1,9 +0,0 @@
-<main role="main" class="container bg-white w-screen max-w-none pt-16">
-    <article class="mx-auto max-w-xl">
-    <h1>Help</h1>
-    <p class="intro">Help will be here!</p>
-    <p>
-        Stuff about help, how to search, etc.
-    </p>
-    </article>
-</main>
