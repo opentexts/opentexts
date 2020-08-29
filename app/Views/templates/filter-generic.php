74212f47ad5227d7274601808bae9525d3845213
Brian Beacom <brizee25@googlemail.com>
2020-08-29 17:08:32 +0100

    Simplify PHP rendering

--- app/Views/templates/filter-generic.php
+++ app/Views/templates/filter-generic.php
@@ -1,12 +1,14 @@
 <?php
 use Solarium\Component\Result\Facet\FacetResultInterface;
-
+const ACTIVE_FILTER_CLASSES = 'text-blue-800 font-semibold';
+const INACTIVE_FILTER_CLASSES = 'text-gray-700';
 function render_facetset(string $key, ?string $currentValue, string $defaultValue, string $multiValue, FacetResultInterface $facet)
 {
     $facetarray = $facet->getValues();
     $values = explode("|", urldecode($currentValue));
+    $defaultActive = $currentValue == null
     ?>
-    <div tabindex="0" class="outline-none cursor-pointer py-2 mr-6 filter">
+    <div data-key="<?= esc($key) ?>" tabindex="0" class="outline-none cursor-pointer py-2 mr-6 filter">
         <div class="flex items-center">
             <span class="text-gray-700 focus:text-blue-700"><?= $currentValue == "" ? $defaultValue : (count($values) > 1 ? $multiValue : $currentValue); ?></span>
             <span class="text-gray-600 pl-1 icon-sm"><?php echo file_get_contents('svg/chevron-down.svg'); ?></span>
@@ -14,56 +16,25 @@ function render_facetset(string $key, ?string $currentValue, string $defaultValu
 
     <div class="rounded-md absolute filter-dropdown overflow-y-auto bg-gray-100 shadow-lg -ml-6">
         <ul class="list-group list-group-flush py-3 pr-3">
-        <?php
-
-            if($currentValue != null)
-            {
-                ?>
-                <li tabindex="0">
-                    <a class="flex items-center p-1 text-gray-700 border-2 border-transparent hover:text-blue-700 focus:text-blue-700 focus:border-blue-500 no-underline" onclick="resetFilter('<?= esc($key) ?>')">
-                        <span class="block w-5"></span>
-                        <?= $defaultValue ?>
-                    </a>
-                </li>
-                <?php
-            }
-            else
-            {
-                ?>
-                <li tabindex="0">
-                    <a class="flex items-center p-1 text-blue-800 font-semibold border-2 border-transparent hover:text-blue-700 focus:text-blue-700 focus:border-blue-500 no-underline" onclick="resetFilter('<?= esc($key) ?>')">
-                        <span class="block text-gray-600 w-5 icon-sm"><?php echo file_get_contents('svg/check.svg'); ?></span>
-                        <?= $defaultValue ?>
-                    </a>
-                </li>
+            <li tabindex="0">
+                <a class="flex items-center p-1 border-2 border-transparent hover:text-blue-700 focus:text-blue-700 focus:border-blue-500 no-underline <?= $defaultActive != null ? ACTIVE_FILTER_CLASSES : INACTIVE_FILTER_CLASSES ?>" onclick="resetFilter('<?= esc($key) ?>')">
+                    <span class="block text-gray-600 w-5 icon-sm <?= $defaultActive ? "" : "invisible" ?>"><?php echo file_get_contents('svg/check.svg'); ?></span>
+                    <span><?= $defaultValue ?></span>
+                </a>
+            </li>
             <?php
-            }
             foreach ($facetarray as $value => $count) {
                 if ($count > 0) {
-                    if(in_array($value, $values))
-                    {
+                    $active = in_array($value, $values);
                     ?>
                         <li tabindex="0">
-                            <a class="flex items-center p-1 text-blue-800 font-semibold border-2 border-transparent hover:text-blue-700 focus:text-blue-700 focus:border-blue-500 no-underline" onclick="removeFilter('<?= esc($key) ?>', '<?= esc($value) ?>')">
-                                <span class="block text-gray-600 w-5 icon-sm"><?php echo file_get_contents('svg/check.svg'); ?></span>
-                                <?= $value ?>
+                            <a class="flex items-center p-1 border-2 border-transparent hover:text-blue-700 focus:text-blue-700 focus:border-blue-500 no-underline <?= $active ? ACTIVE_FILTER_CLASSES : INACTIVE_FILTER_CLASSES ?>" onclick="removeFilter('<?= esc($key) ?>', '<?= esc($value) ?>')">
+                                <span class="block text-gray-600 w-5 icon-sm <?= $active ? "" : "invisible" ?>"><?php echo file_get_contents('svg/check.svg'); ?></span>
+                                <span><?= $value ?></span>
                                 <span class="text-gray-600 font-normal pl-1">(<?= number_format($count); ?>)</span>
                             </a>
                         </li>
                     <?php
-                    }
-                    else
-                    {
-                        ?>
-                        <li tabindex="0">
-                            <a class="flex p-1 text-gray-700 border-2 border-transparent hover:text-blue-700 focus:text-blue-700 focus:border-blue-500 no-underline" onclick="addFilter('<?= esc($key) ?>', '<?= esc($value) ?>')">
-                                <span class="block w-5"></span>
-                                <?= $value ?>
-                                <span class="text-gray-600 pl-1">(<?= number_format($count); ?>)</span>
-                            </a>
-                        </li>
-                        <?php
-                    }
                 }
             }
 
