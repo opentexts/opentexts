cdf4e6e88fbc25e44f09960274c7b22386cd3444
sarah semark <sarah@octopusthink.com>
2020-09-11 02:13:19 +0100

    fix: Copyedit summary text in footer.

--- app/Views/templates/site-footer.php
+++ app/Views/templates/site-footer.php
@@ -12,7 +12,7 @@
                         <img src="/images/open-texts.svg" class="" alt="Open Texts" />
                     </div>
 
-                    <p class="text-xl">OpenTexts.World is an experimental service developed to provide free access to digitised text collections from around the world.</p>
+                    <p class="text-xl">OpenTexts.world provide free access to millions of digitised texts from libraries around the world. Currently in beta.</p>
                 </section>
 
                 <section>
