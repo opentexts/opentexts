1748849ef71ebcacb97b3fdaf7abaa7b4847d6be
sarah semark <sarah@octopusthink.com>
2020-09-12 02:33:35 +0100

    feat: Nix file type icons for plain text.
    Less pretty, more usable.

--- app/Views/templates/search-results.php
+++ app/Views/templates/search-results.php
@@ -41,7 +41,7 @@
     <div class="container mx-auto max-w-xl mb-8">
 
         <!-- Title -->
-        <h2 class="text-darkCyan text-xl leading-tight mb-1">
+        <h2 class="text-darkCyan text-xl leading-tight mb-2">
             <a class="text-blue-700 hover:text-blue-600" rel="bookmark"></a>
         </h2>
 
@@ -56,13 +56,16 @@
             <!-- Year of publication -->
         </span>
 
-        <!-- Icons for different formats -->
-        <div class="inline-flex space-x-1">
-            <a><img src="/images/pdf.png" height="16" width="16" alt="PDF version of this item" /></a>
-            <a><img src="/images/logo-iiif-34x30.png" height="16" width="16" alt="IIIF manifest for this item" /></a>
-            <a><img src="/images/txt.png" height="16" width="16" alt="Plain text version of this item" /></a>
-            <a><img src="/images/alto-xml.png" height="16" width="16" alt="ALTO XML version of this item" /></a>
-            <a><img src="/images/other.png" height="16" width="16" alt="Alternative version of this item" /></a>
+
+
+        <!-- Different formats for download -->
+        <div class="flex flex-wrap items-baseline space-x-1 mt-1 text-gray-600 text-sm">
+            <span>Download:</span>
+            <a class="text-gray-600">PDF</a>
+            <a class="text-gray-600">IIIF</a>
+            <a class="text-gray-600">Plain-text</a>
+            <a class="text-gray-600">ALTO XML</a>
+            <a class="text-gray-600">Alternative</a>
         </div>
 
     </div>
