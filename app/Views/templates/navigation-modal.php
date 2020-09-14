2ad91ca06142a27367d46b306b198a4638089b24
sarah semark <sarah@octopusthink.com>
2020-09-14 14:29:11 +0100

    fix: Add aria tags and screen reader text.

--- app/Views/templates/navigation-modal.php
+++ app/Views/templates/navigation-modal.php
@@ -1,6 +1,7 @@
 <nav id="navigation-modal-wrapper" class="fixed top-0 right-0 bottom-0 z-10 bg-gray-200 p-4 w-full max-w-sm modal-closed">
     <a id="navigation-close" class="w-10 flex ml-auto flex-col justify-center items-center text-gray-700 no-underline cursor-pointer hover:text-blue-700">
-        <span class="text-opacity-50"><?php echo file_get_contents('svg/x.svg'); ?></span>
+        <span aria-hidden="true" class="text-opacity-50"><?php echo file_get_contents('svg/x.svg'); ?></span>
+        <span class="sr-only">Close</span>
     </a>
   <ul class="navigation-modal ml-2">
     <?php
--- app/Views/templates/search-header.php
+++ app/Views/templates/search-header.php
@@ -4,7 +4,7 @@
   <?php include('search-form.php'); ?>
     
     <a id="navigation-toggle" class="flex ml-2 xs:ml-0 flex-col justify-center items-center text-gray-100 no-underline cursor-pointer hover:text-blue-200">
-        <span class="text-opacity-50"><?php echo file_get_contents('svg/menu.svg'); ?></span>
+        <span aria-hidden="true" class="text-opacity-50"><?php echo file_get_contents('svg/menu.svg'); ?></span>
         <span class="text-xs sm:text-sm">Menu</span>
     </a>
   
