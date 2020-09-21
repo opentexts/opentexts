dfb6fa17cc36eea617230039ee36719f2c822bf7
sarah semark <sarah@octopusthink.com>
2020-09-21 23:49:30 +0100

    feat: Update suggested searches with custom images.

--- app/Views/home.php
+++ app/Views/home.php
@@ -10,15 +10,15 @@
 
     <ul class="grid sm:grid-cols-3 gap-10">
       <?php
-        renderSuggestedSearch('medical+report+london', 'medical-reports.svg', 'Medical reports from London');
+        //renderSuggestedSearch('medical+report+london', 'medical-reports.svg', 'Medical reports from London');
         renderSuggestedSearch('new+zealand+volcanoes', 'volcanoes.svg', 'Volcanoes in New Zealand');
-        renderSuggestedSearch('recipe+book', 'recipe-books.svg', 'Recipe books');
+        renderSuggestedSearch('love+poems', 'love-poems.svg', 'Love poems');
+        renderSuggestedSearch('a+midsummer+night%27s+dream', 'midsummer-nights-dream.svg', 'A Midsummer Night’s Dream');
+        //renderSuggestedSearch('recipe+book', 'recipe-books.svg', 'Recipe books');
       ?>
       <!--
-      <li><a href="https://opentexts.world/search?q=a+midsummer+night%27s+dream">A midsummer night's dream</a></li>
       <li><a href="https://opentexts.world/search?q=joke+book">Joke books</a></li>
       <li><a href="https://opentexts.world/search?q=greek+antiquities+dictionary">Greek antiquities dictionary</a></li>
-      <li><a href="https://opentexts.world/search?q=love+poems">Love poems</a></li>
       -->
     </ul>
   </div>
