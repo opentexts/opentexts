35dbd751b0fdb636453c2edc31284aab51d8afc7
Stuart Lewis <stuart@stuartlewis.com>
2020-12-10 06:42:47 +0000

    Highlight correct fields

--- app/Controllers/Search.php
+++ app/Controllers/Search.php
@@ -96,7 +96,7 @@ class Search extends Controller
         $facetSet->createFacetField('langf')->setField('language_facet')->addExclude("filter-lang");
 
         $hl = $query->getHighlighting();
-        $hl->setFields('title, creator, year, publisher, placeOfPublication');
+        $hl->setFields('title, creator, date, publisher, placeOfPublication');
         $hl->setSimplePrefix('<em class="not-italic font-semibold">');
         $hl->setSimplePostfix('</em>');
 
